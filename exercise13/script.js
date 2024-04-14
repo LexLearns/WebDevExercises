45704a94daf10c5f670867c6fedaff6450e2462c
LexLearns <lexlinuxlear@gmail.com>
2024-04-14 21:44:54 +0530

    Exercise 13 completed

---
+++ exercise13/script.js
@@ -0,0 +1,91 @@
+let addCard = confirm("Do you want to add a card");
+
+// function addCard(){
+// 	let card = document.createElement("div");
+// 	card.className = "card";
+// 	card.innerHTML = "<p>This is the added <b>text</b></p>";
+// 	document.querySelector(".container").append(card);
+// }
+
+let viewCountConvertor = views => {
+	if(views > Math.pow(10, 12)){
+		let newCount = Math.round(views/Math.pow(10, 12));
+		return `${newCount}T`
+	}
+	else if(views > Math.pow(10, 9)){
+		let newCount = Math.round(views/Math.pow(10, 9));
+		return `${newCount}B`
+	}
+	else if(views > Math.pow(10, 6)){
+		let newCount = Math.round(views/Math.pow(10, 6));
+		return `${newCount}M`
+	}
+	else if(views > Math.pow(10, 2)){
+		let newCount = Math.round(views/Math.pow(10, 3));
+		return `${newCount}K`
+	}
+	else{
+		return views;
+	}
+}
+
+
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+
+	// This div is for card
+	let card = document.createElement("div");
+	card.className = "card";
+
+	// This div is for thumbnail and the video duration
+	let thumbnailDiv = document.createElement("div");
+	thumbnailDiv.className = "thumbnail";
+
+	let cardThumbnail = document.createElement("img");
+	cardThumbnail.src = thumbnail;
+
+	let cardDuration = document.createElement("p");
+	cardDuration.innerHTML = duration;
+
+	thumbnailDiv.append(cardThumbnail);
+	thumbnailDiv.append(cardDuration);
+
+	// This div is for video title and a div that has channel name, view counts and when it was posted
+	let cardContent = document.createElement("div");
+	cardContent.className = "description";
+
+	let cardTitle = document.createElement("h2");
+	cardTitle.innerHTML = title;
+
+	cardContent.append(cardTitle);
+
+	// This div is for only channel name, view counts and when it was posted
+	let videoDescription = document.createElement("div");
+	videoDescription.className = "counts";
+
+	let cardCName = document.createElement("p");
+	cardCName.innerHTML = cName;
+
+	let cardViewCout = document.createElement("p");
+	cardViewCout.innerHTML = viewCountConvertor(views);
+
+	let cardMonthsOld = document.createElement("p");
+	cardMonthsOld.innerHTML = monthsOld;
+
+	videoDescription.append(cardCName);
+	videoDescription.append(cardViewCout);
+	videoDescription.append(cardMonthsOld);
+
+	cardContent.append(videoDescription);
+
+
+	card.append(thumbnailDiv);
+	card.append(cardContent);
+
+	document.querySelector(".container").append(card);
+}
+
+if(addCard){
+	for(let i = 0; i < 10; i++){
+		createCard("Hello this is the title", "CodeAndEat", 2550501, 345, "13:4", "./asset/cat.jpg");
+	}
+}
